f40b81bcd23513b819f496e6c1e26db76195b24d
dios-214 <dios-214@mail.ru>
2023-05-08 20:59:40 +0700

    Add game field issue#7

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-from tkinter import *
-window = Tk()
-window.title("Пятнашки")
-window.geometry("800x800")
-window.resizable(False, False)
-window.mainloop()
+import tkinter as tk
+import random
+
+# создаем игровое поле
+def create_board():
+	board = [[0] * 4 for _ in range(4)]
+	nums = list(range(1, 16))
+	random.shuffle(nums)
+	nums_iter = iter(nums)
+	for i in range(4):
+		for j in range(4):
+			if i == j == 3:
+				board[i][j] = ''
+			else:
+				board[i][j] = next(nums_iter)
+	return board
+
+# обновляем интерфейс
+def update_board():
+	for i in range(4):
+		for j in range(4):
+			if board[i][j] != '':
+				buttons[i][j].config(text=str(board[i][j]))
+			else:
+				buttons[i][j].config(text='')
+
+# создаем окно и игровое поле
+root = tk.Tk()
+root.title('Пятнашки')
+
+board = create_board()
+
+buttons = [[0] * 4 for _ in range(4)]
+for i in range(4):
+	for j in range(4):
+		button = tk.Button(root, width=4, height=2, font=('Arial', 20, 'bold'), command=lambda row=i, col=j: get_move(row, col))
+		button.grid(row=i, column=j)
+		buttons[i][j] = button
+
+message = tk.Label(root, text='', font=('Arial', 14))
+message.grid(row=4, column=0, columnspan=4)
+
+update_board()
+
+root.mainloop()
